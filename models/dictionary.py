701d23da40f73f20b420f3269874e548c542fbaf
zkatemor <rinaholmes7@gmail.com>
2020-04-13 19:38:38 +0300

    fix alembic error

--- models/dictionary.py
+++ models/dictionary.py
@@ -30,21 +30,19 @@ class Word(session.Model):
     __tablename__ = 'word'
     id = session.Column(Integer, primary_key=True, autoincrement=True)
     word = session.Column(String(120), nullable=False)
-    part_of_speech = session.Column(String(50), nullable=False)
     tone = session.Column(String(50), nullable=False, default='None')
 
     dictionaryId = session.Column(Integer, ForeignKey('dictionary.id'), nullable=False)
     dictionary = session.relationship('Dictionary', backref=session.backref('words', lazy=True))
 
-    def __init__(self, dictionaryId, word, tone, part_of_speech):
+    def __init__(self, dictionaryId, word, tone):
         self.dictionaryId = dictionaryId
         self.word = word
         self.tone = tone
-        self.part_of_speech = part_of_speech
 
     def __repr__(self):
-        return "<Word(id={}, word={}, tone={}, part_of_speech={}".format(
-            self.id, self.word, self.tone, self.part_of_speech
+        return "<Word(id={}, word={}, tone={}".format(
+            self.id, self.word, self.tone
         )
 
     def __str__(self):
@@ -55,6 +53,5 @@ class Word(session.Model):
             'id': self.id,
             'word': self.word,
             'tone': self.tone,
-            'part_of_speech': self.part_of_speech,
             'dictionary_id': self.dictionaryId
         }
