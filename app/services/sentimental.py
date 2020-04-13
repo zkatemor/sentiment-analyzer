d06cb1033a04f2ab6b4f215f8803d4f0f63d1525
zkatemor <rinaholmes7@gmail.com>
2020-04-14 00:01:05 +0300

    added simple lexicon classifier

---
+++ app/services/sentimental.py
@@ -0,0 +1,84 @@
+import re
+import os
+import csv
+from collections import defaultdict
+
+
+class Sentimental(object):
+    def __init__(self, dictionary=None, negation=None):
+        if dictionary is None and negation is None:
+            base_dir = os.path.dirname(__file__)
+            dictionary = [os.path.join(base_dir, p) for p in ['dictionaries/rusentilex.csv']]
+            negation = os.path.join(base_dir, 'dictionaries/negations.csv')
+
+        self.dictionary = {}
+        self.negations = set()
+
+        for wl_filename in self.__to_arg_list(dictionary):
+            self.load_dictionary(wl_filename)
+        for negations_filename in self.__to_arg_list(negation):
+            self.load_neagations(negations_filename)
+
+        self.__negation_skip = {'a', 'an', 'so', 'too'}
+
+    @staticmethod
+    def __to_arg_list(obj):
+        if obj is not None:
+            if not isinstance(obj, list):
+                obj = [obj]
+        else:
+            obj = []
+        return obj
+
+    def __is_prefixed_by_negation(self, token_idx, tokens):
+        #   True if i != 0 and tokens[i - 1] in self.negations else False
+        prev_idx = token_idx - 1
+        if tokens[prev_idx] in self.__negation_skip:
+            prev_idx -= 1
+
+        is_prefixed = False
+        if token_idx > 0 and prev_idx >= 0 and tokens[prev_idx] in self.negations:
+            is_prefixed = True
+
+        return is_prefixed
+
+    def load_neagations(self, filename):
+        with open(filename, 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            negations = set([row['token'] for row in reader])
+        self.negations |= negations
+
+    def load_dictionary(self, filename):
+        with open(filename, 'r',  encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            dictionary = {row['term']: row['tone'] for row in reader}
+        self.dictionary.update(dictionary)
+
+    def analyze(self, sentence):
+        sentence_clean = re.sub(r'[^\w ]', ' ', sentence.lower())
+        tokens = sentence_clean.split()
+
+        scores = defaultdict(float)
+        words = defaultdict(list)
+        comparative = 0
+
+        for i, token in enumerate(tokens):
+            is_prefixed_by_negation = self.__is_prefixed_by_negation(i, tokens)
+            if token in self.dictionary and not is_prefixed_by_negation:
+                tone = self.dictionary[token]
+
+                score = 1 if tone == 'positive' else -1
+                scores[tone] += score
+                words[tone].append(token)
+
+        if len(tokens) > 0:
+            comparative = (scores['positive'] + scores['negative']) / len(tokens)
+
+        result = {
+            'score': scores['positive'] + scores['negative'],
+            'positive': scores['positive'],
+            'negative': scores['negative'],
+            'comparative': comparative,
+        }
+
+        return result
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import render_template
+from flask import render_template, request, redirect
 from flask_restful import Api
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -7,6 +7,7 @@ from flask_migrate import Migrate, MigrateCommand
 from app import create_app
 from app.api.views import DictionaryView, WordView
 from db import session
+from app.services.sentimental import Sentimental
 
 load_dotenv()
 
@@ -25,7 +26,25 @@ api.add_resource(WordView, '/word')
 @app.route('/sentiment', methods=['GET'])
 def index():
     """home page"""
-    return render_template("index.html")
+    result = {
+        'score': 0,
+        'positive': 0,
+        'negative': 0,
+        'comparative': 0,
+    }
+    text = "Сегодня не плохой день, а очень хороший!"
+
+    return render_template("index.html", text=text, result=result)
+
+
+@app.route('/sentiment/analyzer', methods=['POST'])
+def classifier():
+    text = request.form['text']
+
+    sent = Sentimental(dictionary=['app/dictionaries/rusentilex.csv'],
+                       negation='app/dictionaries/negations.csv')
+    result = sent.analyze(text)
+    return render_template("index.html", text=text, result=result)
 
 
 if __name__ == '__main__':
