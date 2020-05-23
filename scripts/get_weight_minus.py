ef6fd27d5761982e41f0685e16a5313e4c269b3e
zkatemor <rinaholmes7@gmail.com>
2020-05-24 01:06:44 +0300

    added weight to dict (chi-square)

---
+++ scripts/get_weight_minus.py
@@ -0,0 +1,26 @@
+"""формирование весов слов для отлицательного словаря хи-квадрат"""
+import csv
+from itertools import zip_longest
+import numpy as np
+import pandas as pd
+
+
+def scale(chi_square):
+    return -1 * (chi_square - np.min(chi_square)) / (np.max(chi_square) - np.min(chi_square))
+
+
+df = pd.read_csv('D:\\GitHub\\sentiment-analyzer\\app\\dictionaries\\dict_chi_minus.csv', encoding='utf-8')
+chi = np.array(df['chi_square'])
+chi_scaled = scale(chi)
+
+words = df['term'].tolist()
+
+d = [words, chi_scaled]
+export_data = zip_longest(*d, fillvalue='')
+
+with open('D:\\GitHub\\sentiment-analyzer\\app\\dictionaries\\weight_chi_minus.csv', 'w', encoding='utf-8',
+          newline='') as f:
+    wr = csv.writer(f)
+    wr.writerow(("term", "weight"))
+    wr.writerows(export_data)
+f.close()
---
+++ scripts/get_weight_plus.py
@@ -0,0 +1,26 @@
+"""формированиен весов слов для положительного словаря хи-квадрат"""
+import csv
+from itertools import zip_longest
+import numpy as np
+import pandas as pd
+
+
+def scale(chi_square):
+    return (chi_square - np.min(chi_square)) / (np.max(chi_square) - np.min(chi_square))
+
+
+df = pd.read_csv('D:\\GitHub\\sentiment-analyzer\\app\\dictionaries\\dict_chi_plus.csv', encoding='utf-8')
+chi = np.array(df['chi_square'])
+chi_scaled = scale(chi)
+
+words = df['term'].tolist()
+
+d = [words, chi_scaled]
+export_data = zip_longest(*d, fillvalue='')
+
+with open('D:\\GitHub\\sentiment-analyzer\\app\\dictionaries\\weight_chi_plus.csv', 'w', encoding='utf-8',
+          newline='') as f:
+    wr = csv.writer(f)
+    wr.writerow(("term", "weight"))
+    wr.writerows(export_data)
+f.close()
