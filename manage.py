5849a83c0621dfcf82fcf762354aecf9fcb7d602
zkatemor <rinaholmes7@gmail.com>
2020-05-27 00:23:55 +0300

    fix menu & added list of dictionary

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import csv
+
 from dotenv import load_dotenv
 from flask import render_template, request, redirect
 from flask_restful import Api
@@ -31,7 +33,7 @@ def index():
         'positive': 0,
         'negative': 0,
         'comparative': 0,
-        'words': {"positive": "ldld"}
+        'words': {}
     }
     text = "Сегодня день не плохой, даже не ужасный, а очень хороший!"
     img = 'hand.png'
@@ -39,7 +41,7 @@ def index():
     return render_template("index.html", text=text, result=result, img=img)
 
 
-@app.route('/sentiment/analyzer', methods=['POST'])
+@app.route('/analyze', methods=['POST'])
 def classifier():
     text = request.form['text']
 
@@ -53,6 +55,35 @@ def classifier():
     else:
         img = 'negative.png'
     return render_template("index.html", text=text, result=result, img=img)
+
+
+@app.route('/dictionaries/<name>', methods=['GET'])
+def get_dictionaries(name):
+    about_dictionary = ""
+    dictionary = {}
+    if name == 'chi_unigram':
+        about_dictionary = "Словарь униграм, построенный с помощью критерия согласия Пирсона (Хи-квадрат)"
+        with open('app/dictionaries/chi_plus.csv', encoding='utf-8') as f:
+            dictionary = dict(filter(None, csv.reader(f)))
+        dictionary.pop('term')
+    elif name == 'cnn_unigram':
+        about_dictionary = "Словарь униграм, построенный с помощью свёрточной нейронной сети"
+    elif name == 'rusentilex':
+        about_dictionary = "Словарь оценочных слов и выражений русского языка РуСентиЛекс (" \
+                           "http://www.labinform.ru/pub/rusentilex/rusentilex_2016.txt) "
+    elif name == 'chi_bigram':
+        about_dictionary = "Словарь биграм, построенный с помощью критерия согласия Пирсона (Хи-квадрат)"
+    return render_template("dictionaries.html", about_dictionary=about_dictionary, dictionary=dictionary)
+
+
+@app.route('/about', methods=['GET'])
+def about_project():
+    return render_template("about.html")
+
+
+@app.route('/analyze/files', methods=['GET'])
+def files_classifier():
+    return render_template("files_analyze.html")
 
 
 if __name__ == '__main__':
